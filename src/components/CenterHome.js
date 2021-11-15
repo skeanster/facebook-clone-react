b50dbfa5b9f3df7cd689aa1bceface8cd4ac0b27
skeanster <ryanskeans@gmail.com>
2021-11-15 12:27:54 -0800

    Home center component style improvements

--- src/components/CenterHome.js
+++ src/components/CenterHome.js
@@ -1,8 +1,20 @@
+import ProfilePic from './miscimages/default-profile.png';
+import { Link } from 'react-router-dom';
+
 function CenterHome(props) {
   const { username, posts } = props;
   return (
     <div className="centerHomeContainer">
-      <div className="newPostContainer">What's on your mind, {username}?</div>
+      <div className="newPostContainer">
+        <div className="newPostMainContainer">
+          <Link to="/facebook-clone-react/ProfilePage">
+            <img src={ProfilePic} alt="profile pic" className="ProfileImage" />
+          </Link>
+          <div className="mainNewPostButton">
+            What's on your mind, {username}?
+          </div>
+        </div>
+      </div>
       {posts.map((post) => {
         return (
           <div className="postContainer" key={post.poster + post.post}>
