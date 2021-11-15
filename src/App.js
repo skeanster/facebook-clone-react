e3e367f7aff8c9fc35f158ba39ce15829fe907e7
skeanster <ryanskeans@gmail.com>
2021-11-15 10:39:32 -0800

    added left side component on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import LeftHome from './components/LeftHome';
 
 function App(props) {
   const { username } = props;
   return (
     <div className="App">
       <Navbar username={username} />
+      <div className="homeMainContainer">
+        <LeftHome username={username} />
+      </div>
     </div>
   );
 }
---
+++ src/components/LeftHome.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+import ProfilePic from './miscimages/default-profile.png';
+import FriendsPic from './LeftHomeCompontents/friends.png';
+import EventsPic from './LeftHomeCompontents/events.png';
+import GroupsPic from './LeftHomeCompontents/groups.png';
+import MarketplacePic from './LeftHomeCompontents/marketplace.png';
+import WatchPic from './LeftHomeCompontents/watch.png';
+import MemoriesPic from './LeftHomeCompontents/memories.png';
+import SavedPic from './LeftHomeCompontents/saved.png';
+import PagesPic from './LeftHomeCompontents/pages.png';
+import NewsPic from './LeftHomeCompontents/news.png';
+
+function LeftHome(props) {
+  const { username } = props;
+  return (
+    <div className="leftHomeContainer">
+      <ul className="leftHomeList">
+        <Link to="/ProfilePage" className="leftHomeListItem">
+          <img src={ProfilePic} alt="profile pic" className="ProfileImage" />
+          <div className="leftHomeText">{username}</div>
+        </Link>
+        <li className="leftHomeListItem">
+          <img src={FriendsPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Friends</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={EventsPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Events</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={GroupsPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Groups</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img
+            src={MarketplacePic}
+            alt="profile pic"
+            className="leftHomeImage"
+          />
+          <div className="leftHomeText">Marketplace</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={WatchPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Watch</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={MemoriesPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Memories</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={SavedPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Saved</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={PagesPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">Pages</div>
+        </li>
+        <li className="leftHomeListItem">
+          <img src={NewsPic} alt="profile pic" className="leftHomeImage" />
+          <div className="leftHomeText">News</div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default LeftHome;
