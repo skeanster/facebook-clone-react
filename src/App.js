637d0ced45ed27ed618f871f2539d1cbb72c2245
Ryan Skeans <ryanskeans@gmail.com>
2025-06-17 13:01:56 -0700

    make tab home blue when active

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import Navbar from './components/Navbar';
-import LeftHome from './components/LeftHome';
-import CenterHome from './components/CenterHome';
-import RightHome from './components/RightHome';
-import InfoButton from './components/infoButton';
+import Navbar from "./components/Navbar";
+import LeftHome from "./components/LeftHome";
+import CenterHome from "./components/CenterHome";
+import RightHome from "./components/RightHome";
+import InfoButton from "./components/infoButton";
 
 function App(props) {
-  const { username, posts, newPost, like, newComment, checkLike } = props;
+    const { username, posts, newPost, like, newComment, checkLike } = props;
 
-  return (
-    <div className="App">
-      <Navbar username={username} />
-      <div className="homeMainContainer">
-        <LeftHome username={username} />
-        <CenterHome
-          username={username}
-          posts={posts}
-          newPost={newPost}
-          like={like}
-          newComment={newComment}
-          checkLike={checkLike}
-        />
-        <RightHome />
-      </div>
-      <InfoButton />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar username={username} active={true} />
+            <div className="homeMainContainer">
+                <LeftHome username={username} />
+                <CenterHome
+                    username={username}
+                    posts={posts}
+                    newPost={newPost}
+                    like={like}
+                    newComment={newComment}
+                    checkLike={checkLike}
+                />
+                <RightHome />
+            </div>
+            <InfoButton />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,91 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as LogoSvg } from './NavbarComponents/logo.svg';
-import { ReactComponent as GamingSvg } from './NavbarComponents/gaming.svg';
-import { ReactComponent as HomeSvg } from './NavbarComponents/home.svg';
-import { ReactComponent as WatchSvg } from './NavbarComponents/watch.svg';
-import { ReactComponent as StoreSvg } from './NavbarComponents/store.svg';
-import { ReactComponent as DotsSvg } from './NavbarComponents/dots.svg';
-import { ReactComponent as MessengerSvg } from './NavbarComponents/messenger.svg';
-import { ReactComponent as NotificationsSvg } from './NavbarComponents/notifications.svg';
-import { ReactComponent as ArrowSvg } from './NavbarComponents/arrow.svg';
-import Search from './NavbarComponents/Search';
-import ProfilePic from './miscimages/default-profile.png';
+import { Link } from "react-router-dom";
+import { ReactComponent as LogoSvg } from "./NavbarComponents/logo.svg";
+import { ReactComponent as GamingSvg } from "./NavbarComponents/gaming.svg";
+import { ReactComponent as HomeSvg } from "./NavbarComponents/home.svg";
+import { ReactComponent as WatchSvg } from "./NavbarComponents/watch.svg";
+import { ReactComponent as StoreSvg } from "./NavbarComponents/store.svg";
+import { ReactComponent as DotsSvg } from "./NavbarComponents/dots.svg";
+import { ReactComponent as MessengerSvg } from "./NavbarComponents/messenger.svg";
+import { ReactComponent as NotificationsSvg } from "./NavbarComponents/notifications.svg";
+import { ReactComponent as ArrowSvg } from "./NavbarComponents/arrow.svg";
+import Search from "./NavbarComponents/Search";
+import ProfilePic from "./miscimages/default-profile.png";
 
 const Navbar = (props) => {
-  const { username } = props;
+    const { username, active } = props;
 
-  return (
-    <div className="navbarContainer">
-      <div className="navbarLeftContainer">
-        <Link to="/facebook-clone-react" className="logo">
-          <LogoSvg />
-        </Link>
-        <Search />
-      </div>
-      <div className="navbarCenterContainer" id="navbarCenterContainer">
-        <Link
-          to="/facebook-clone-react"
-          className="navbarCenterIcons"
-          id="home"
-        >
-          <HomeSvg />
-        </Link>
-        <div className="navbarCenterIcons" id="watch">
-          <WatchSvg />
+    return (
+        <div className="navbarContainer">
+            <div className="navbarLeftContainer">
+                <Link to="/facebook-clone-react" className="logo">
+                    <LogoSvg />
+                </Link>
+                <Search />
+            </div>
+            <div className="navbarCenterContainer" id="navbarCenterContainer">
+                <Link
+                    to="/facebook-clone-react"
+                    className={`navbarCenterIcons ${
+                        active ? "active-nav" : ""
+                    }`}
+                    id="home"
+                >
+                    <HomeSvg />
+                </Link>
+                <div className="navbarCenterIcons" id="watch">
+                    <WatchSvg />
+                </div>
+                <div className="navbarCenterIcons" id="store">
+                    <StoreSvg />
+                </div>
+                <div className="navbarCenterIcons" id="gaming">
+                    <GamingSvg />
+                </div>
+            </div>
+            <div className="navbarRightContainer">
+                <Link
+                    to="/facebook-clone-react/ProfilePage"
+                    className="navbarProfileLink"
+                    id="navbarProfileLink"
+                >
+                    <img
+                        src={ProfilePic}
+                        alt="profile pic"
+                        className="ProfileImage"
+                    />
+                    <div className="profileName">{username}</div>
+                </Link>
+                <Link
+                    to="/facebook-clone-react/ProfilePage"
+                    className="navbarRightLinks"
+                    id="navbarMenu"
+                >
+                    <DotsSvg />
+                </Link>
+                <Link
+                    to="/facebook-clone-react/ProfilePage"
+                    className="navbarRightLinks"
+                    id="navbarMessenger"
+                >
+                    <MessengerSvg />
+                </Link>
+                <Link
+                    to="/facebook-clone-react/ProfilePage"
+                    className="navbarRightLinks"
+                    id="navbarNotifications"
+                >
+                    <NotificationsSvg />
+                </Link>
+                <Link
+                    to="/facebook-clone-react/ProfilePage"
+                    className="navbarRightLinks"
+                    id="navbarExpand"
+                >
+                    <ArrowSvg className="arrow-svg" />
+                </Link>
+            </div>
         </div>
-        <div className="navbarCenterIcons" id="store">
-          <StoreSvg />
-        </div>
-        <div className="navbarCenterIcons" id="gaming">
-          <GamingSvg />
-        </div>
-      </div>
-      <div className="navbarRightContainer">
-        <Link
-          to="/facebook-clone-react/ProfilePage"
-          className="navbarProfileLink"
-          id="navbarProfileLink"
-        >
-          <img src={ProfilePic} alt="profile pic" className="ProfileImage" />
-          <div className="profileName">{username}</div>
-        </Link>
-        <Link
-          to="/facebook-clone-react/ProfilePage"
-          className="navbarRightLinks"
-          id="navbarMenu"
-        >
-          <DotsSvg />
-        </Link>
-        <Link
-          to="/facebook-clone-react/ProfilePage"
-          className="navbarRightLinks"
-          id="navbarMessenger"
-        >
-          <MessengerSvg />
-        </Link>
-        <Link
-          to="/facebook-clone-react/ProfilePage"
-          className="navbarRightLinks"
-          id="navbarNotifications"
-        >
-          <NotificationsSvg />
-        </Link>
-        <Link
-          to="/facebook-clone-react/ProfilePage"
-          className="navbarRightLinks"
-          id="navbarExpand"
-        >
-          <ArrowSvg />
-        </Link>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
